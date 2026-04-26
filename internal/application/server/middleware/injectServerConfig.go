aada89a5a84f6acf355677ea0dbf4afca45bed00
Kirill K <agrrh@users.noreply.github.com>
2026-04-26 14:09:58 +0300

    fix: remove wrong comments (#19)

--- internal/application/server/middleware/injectServerConfig.go
+++ internal/application/server/middleware/injectServerConfig.go
@@ -6,7 +6,6 @@ import (
 	"github.com/agrrh/mycorp/internal/application/server/config"
 )
 
-// ServerHeader middleware adds a `Server` header to the response.
 func InjectServerConfig(cfg config.Config) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(ctx echo.Context) error {
